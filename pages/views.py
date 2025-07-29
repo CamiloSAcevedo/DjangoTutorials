5b98e359fb3e4e6bad24508129822593f458dbfe
CamiloSAcevedo <cami021807@gmail.com>
2025-07-29 17:55:44 -0500

    Se actualiza todo hasta el about page

--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,18 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name='pages/home.html'
+
+class AboutPageView(TemplateView):
+    template_name='pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title" : "About us - Online Store",
+            "subtitle" : "About us",
+            "description" : "This is an About page...",
+            "author" : "Developed by: Camilo Salazar",
+        })
+        return context
+    
+
