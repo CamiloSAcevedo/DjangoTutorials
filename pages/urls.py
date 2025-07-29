c84936508aa73919fffde080049bace335076c1d
CamiloSAcevedo <cami021807@gmail.com>
2025-07-29 14:06:00 -0500

    se añade los html

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import homePageView
+from .views import HomePageView
 
 urlpatterns = [
-    path ("", homePageView, namee ='home'),
+    path ("", HomePageView.as_view(), name ='home'),
 ]
